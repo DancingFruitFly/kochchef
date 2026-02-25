bb6f83e66320d5d4a21f3b81ad1f7e7aef332d99
Jemoa <jemaine.it12c@eeez.de>
2026-02-25 14:35:50 +0100

    docker structure change

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,34 +1,17 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ServeStaticModule } from '@nestjs/serve-static';
-import path from 'path';
 import { CounterModule } from './counter/counter.module';
+import { join } from 'path';
 
 @Module({
   imports: [
     CounterModule,
-    ServeStaticModule.forRootAsync({
-      imports: [ConfigModule],
-      useFactory: (configService: ConfigService) => [
-        {
-          rootPath: path.join(
-            __dirname,
-            '..',
-            configService.get<string>('STATIC_FOLDER') || '../client/dist',
-          ),
-        },
-      ],
-      inject: [ConfigService],
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+      exclude: ['/api*'],
     }),
-    MongooseModule.forRootAsync({
-      imports: [ConfigModule],
-      useFactory: async (configService: ConfigService) => ({
-        uri: configService.get<string>('MONGODB_URI'),
-      }),
-      inject: [ConfigService],
-    }),
-    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGO_URI as string),
   ],
   controllers: [],
   providers: [],
